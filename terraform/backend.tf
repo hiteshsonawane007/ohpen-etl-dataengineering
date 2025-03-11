78a02dca8d89c45439ac49395d471affbbb66e0c
Hitesh <hitesh.sonawane007@gmail.com>
2025-03-11 19:10:54 +0530

    Change AWS Regiion

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "hiteshnlams-demos-001"
     key    = "terraform/state/ohpen-etl-project/terraform.tfstate"
-    region = "us-east-1"
+    region = "eu-north-1"
     use_lockfile = true
     encrypt = true
   }
