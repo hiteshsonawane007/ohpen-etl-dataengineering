c273fc36a9e88d5262fa997d96b9dd86cf928eca
Hitesh <hitesh.sonawane007@gmail.com>
2025-03-12 12:29:31 +0100

    Glue Crawler IAM Role.

--- terraform/modules/glue_crawler/main.tf
+++ terraform/modules/glue_crawler/main.tf
@@ -6,17 +6,18 @@ resource "aws_glue_crawler" "this" {
   name          = var.crawler_name
   role          = var.role_arn
   database_name = aws_glue_catalog_database.this.name
-  targets {
-    s3_targets {
-      path = var.s3_target
-    }
+
+  s3_target {
+    path = var.s3_target
   }
+
   configuration = jsonencode({
-    Version = 1.0,
-    CrawlerOutput = {
-      Partitions = {
+    Version         = 1.0,
+    CrawlerOutput   = {
+      Partitions  = {
         AddOrUpdateBehavior = "InheritFromTable"
       }
     }
   })
 }
+
