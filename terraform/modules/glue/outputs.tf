8e1e011c4bda7e0012ea8ee70302b53a85f63bed
Hitesh <hitesh.sonawane007@gmail.com>
2025-03-12 12:22:32 +0100

    Glue Crawler IAM Role.

--- terraform/modules/glue/outputs.tf
+++ terraform/modules/glue/outputs.tf
@@ -2,3 +2,7 @@ output "etl_job_arn" {
   description = "ARN of the Glue ETL job"
   value       = aws_glue_job.this.arn
 }
+output "glue_role_arn" {
+  description = "ARN of the IAM role used by the Glue job"
+  value       = aws_iam_role.glue_role.arn
+}
